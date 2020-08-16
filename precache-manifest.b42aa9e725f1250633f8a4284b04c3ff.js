b732c5b835dce66cbf65536ecdcc7de202f60b38
Ayrahcamrak <sudip.k.charya@gmail.com>
2020-08-16 16:26:58 -0400

    Updates

---
+++ precache-manifest.b42aa9e725f1250633f8a4284b04c3ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71d212b6670216346e59990d8816c01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5659fed113ad45e80f1",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "27d4cf72dfaabc40e032",
+    "url": "/static/js/2.69ba2d3f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.69ba2d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5659fed113ad45e80f1",
+    "url": "/static/js/main.b96348cc.chunk.js"
+  },
+  {
+    "revision": "5956faf3435c5dc3dcca",
+    "url": "/static/js/runtime-main.f3f0f1e3.js"
+  }
+]);
